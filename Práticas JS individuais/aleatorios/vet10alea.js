79996a61ea3ee5ec38eeba67d3c329880ba59875
Vanilson Sousa <vanilsonped7@gmail.com>
2023-03-27 16:51:59 -0300

    Update vet10alea.js

--- Práticas JS individuais/aleatorios/vet10alea.js
+++ Práticas JS individuais/aleatorios/vet10alea.js
@@ -3,4 +3,4 @@ let num = []
 for(var c = 1; c <= 10; c++){
     num.push(parseInt(Math.random() * 11))
 }
-console.log(`Os números pseudo aleatórios gerados foram ${num}.`)
+console.log(`Os números pseudoaleatórios gerados foram ${num}.`)
