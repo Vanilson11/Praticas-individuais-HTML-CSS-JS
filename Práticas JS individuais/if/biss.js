b139bbd24c7907fc3c8834b6c7affa16929cefdf
Vanilson Sousa <vanilsonped7@gmail.com>
2023-03-29 15:47:50 -0300

    reorganização dos exercícios

--- Práticas JS individuais/if/biss.js
+++ Práticas JS individuais/if/biss.js
@@ -1,5 +1,5 @@
 var ano = 2000
-if (ano % 4 == 0) & (ano % 100 == 0) || (ano % 400 == 0){
+if (ano % 4 == 0 || ano % 100 == 0 || ano % 400 == 0){
     console.log('O ano é Bissexto')
 } else {
     console.log('O ano não é Bissexto')
